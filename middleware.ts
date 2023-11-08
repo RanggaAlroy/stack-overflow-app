fb52afc190a1a577614c03ccc43c54f8d5f77aec
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2023-11-08 12:59:45 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     '/',
-    '/api/webhook/route',
+    '/api/webhook/',
     'question/:id',
     '/tags',
     '/tags/:id',
